029d4267d4974687bd38f4e35dcb1cb0495b09e3
Richard Harrison <rjh@zaretto.com>
2025-03-31 14:48:07 +0100

    Rework switch in Run
    This is just for clarity; the actual code doesn't change.

--- src/FGFDMExec.cpp
+++ src/FGFDMExec.cpp
@@ -421,30 +421,27 @@ bool FGFDMExec::Run(void)
   if (Script && !IntegrationSuspended()) success = Script->RunScript();
 
   for (unsigned int i = 0; i < Models.size(); i++) {
-    switch (i)
-    {
-        //      case  ePropagate:
-    case  eSystems:
-        LoadInputs(i);
-        Models[i]->Run(holding);
-        break;
-    case  eInput:
-        //      case  eInertial:
-        //      case  eAtmosphere:
-        //      case  eWinds:
-              case  eMassBalance:
-        //      case  eAuxiliary:
-    case  ePropulsion:
-    case  eAerodynamics:
-    case  eGroundReactions:
-    case  eExternalReactions:
-        //      case  eBuoyantForces:
-    case  eAircraft:
-              case  eAccelerations:
-    case  eOutput:
-        LoadInputs(i);
-        Models[i]->Run(holding);
-        break;
+    switch (i) { 
+      // For DCS we only need a subset of the models to run.
+      //      case  eAtmosphere:
+      //      case  eAuxiliary:
+      //      case  eBuoyantForces:
+      //      case  eInertial:
+      //      case  ePropagate:
+      //      case  eWinds:
+      case eAccelerations:
+      case eAerodynamics:
+      case eAircraft:
+      case eExternalReactions:
+      case eGroundReactions:
+      case eInput:
+      case eMassBalance:
+      case eOutput:
+      case ePropulsion:
+      case eSystems:
+          LoadInputs(i);
+          Models[i]->Run(holding);
+          break;
 
     default:
         break; // For DCS we only need a subset of the models to run.
